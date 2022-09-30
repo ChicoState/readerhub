93949da3c128bcb2a7f9570aefabf99b30b9971c
Aryan <mittalaryan99@gmail.com>
2022-09-29 21:19:36 -0700

    added posts functionality - add, edit, delete - for local user only

--- posts/views.py
+++ posts/views.py
@@ -9,4 +9,62 @@ from posts.forms import PostForm
 
 @login_required(login_url='/login/')
 def posts(request):
+    if (request.method == "GET" and "delete" in request.GET):
+        id = request.GET["delete"]
+        Post.objects.filter(id=id).delete()
+        return redirect("/posts/")
+    else:
+        posts = Post.objects.filter(user=request.user)
+        context = {
+            "posts": posts,
+        }
+    return render(request, 'posts/posts.html', context)
 
+@login_required(login_url='/login/')
+def add_post(request):
+    if (request.method == "POST"):
+        if ("add" in request.POST):
+            form = PostForm(request.POST)
+            if (form.is_valid()):
+                newPost = form.save(commit=False)
+                newPost.user = request.user
+                newPost.save()
+                return redirect("/posts/")
+            else:
+                context = {
+                    "form_data": form
+                }
+                return render(request, "posts/add_post.html", context)
+        else:
+            return redirect("/posts/")
+    else:
+        context = {
+            "form_data": PostForm()
+        }
+        return render(request, "posts/add_post.html", context)
+
+@login_required(login_url="/login/")
+def edit_post(request, id):
+    if (request.method == "GET"):
+        item = Post.objects.get(id=id)
+        form = PostForm(instance=item)
+        context = {
+            "form_data": form
+        }
+        return render(request, "posts/edit.html", context)
+    elif (request.method == "POST"):
+        if ("edit" in request.POST):
+            form = PostForm(request.POST)
+            if (form.is_valid()):
+                post = form.save(commit=False)
+                post.user = request.user
+                post.id = id
+                post.save()
+                return redirect("/posts/")
+            else:
+                context = {
+                    "form_data": form
+                }
+                return render(request, "posts/add_post.html", context)
+        else:
+            return redirect("/posts/")
--- readerhub/urls.py
+++ readerhub/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from app1 import views as app1_views
 from personalization import views as personalization_views
+from posts import views as post_views
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -27,8 +28,10 @@ urlpatterns = [
     path('login/', app1_views.user_login),
     path('logout/', app1_views.user_logout),
     path('personalization/', personalization_views.personalization),
-    path('personalization/add_profile/', personalization_views.add_profile),
     path('personalization/edit_profile/<int:id>/', personalization_views.edit_profile),
+    path('posts/', post_views.posts),
+    path('posts/add_post/', post_views.add_post),
+    path('posts/edit_post/<int:id>/', post_views.edit_post),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #needed to save images to static
