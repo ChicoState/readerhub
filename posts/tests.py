6545af522818061b0285c49e9216c549375094bf
Aryan <mittalaryan99@gmail.com>
2022-12-04 15:38:08 -0800

    fixed basic posts tests

--- posts/tests.py
+++ posts/tests.py
@@ -6,7 +6,7 @@ from posts.models import Post
 # Create your tests here.
 class PostTest(TestCase):
     def setUp(self):
-        self.testUser = User.object.create_user("John Doe", "johndoe@gmail.com", "johndoe")
+        self.testUser = User.objects.create_user("John Doe", "johndoe@gmail.com", "johndoe")
         FavoriteBooks.objects.create(favorite_user=self.testUser, favorite_id="testid", favorite_title="testtitle", favorite_cover="testcover")
         self.testPost = Post.objects.create(title="post test", content="test content", user=self.testUser, book_object=FavoriteBooks.objects.get(favorite_id="testid"))
 
