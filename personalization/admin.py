15cf3d7a88bf5724b963a8c4235de9dfe9ec0091
Elijah Willis <ejwillis@mail.csuchico.edu>
2022-12-09 15:36:15 -0800

    updated book view

--- personalization/admin.py
+++ personalization/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Follows
 from .models import Critic
 from .models import PersonalInfo
+from .models import FavoriteBooks
 
 # Register your models here.
 admin.site.register(Follows)
 admin.site.register(Critic)
 admin.site.register(PersonalInfo)
+admin.site.register(FavoriteBooks)
